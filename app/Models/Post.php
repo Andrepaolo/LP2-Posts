325fc0406041796a36065c7c265d6e06796e532a
Andrepaolo <andrepaol32@gmail.com>
2022-04-21 12:49:13 -0500

    21 abril

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,7 +15,7 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
     public function tags(){
-        return $this->belongsToMany(Tags::class);
+        return $this->belongsToMany(Tag::class);
 
     }
     public function images(){
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+       Storage::makeDirectory('posts');
+       $this->call(UserSeeder::class);
+       Category::factory(4)->create();
+       Tag::factory(8)->create();
+       $this->call(PostSeeder::class);
+
+
+
+
+
         // \App\Models\User::factory(10)->create();
     }
 }
